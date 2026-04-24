c952587f6347e6493cd0245ad7bc7e2aa1310bfa
sevinc995 <sevincseferova25@gmail.com>
2026-04-24 12:37:04 +0000

    created

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 "use strict";
 $(document).ready(function () {
-
+    document.getElementById("form").addEventListener("submit", function (e) {
+        e.preventDefault();
+        alert("Qeydiyyat uğurludur!");
+        console.log("Site ready");
+    });
 });
